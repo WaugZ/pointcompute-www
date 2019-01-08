89bc66cbbe96195ba919c17cc9e4424c5f909b38
wangzi <waugz@icloud.com>
2019-01-08 15:50:58 +0800

    logic bug fix

--- public/compute.js
+++ public/compute.js
@@ -4,21 +4,23 @@ app.controller('myCtr', function($scope) {
     $scope.loserScore;
     $scope.winnerGet;
     $scope.loserLose;
-    $scope.winner;
-    $scope.losser;
+    $scope.winner = "\n";
+    $scope.losser = "\n";
     $scope.calculate = function() {
     	$scope.winner = "胜者得分";
     	$scope.losser = "败者失分";
-    	var thesholds = new Array(13, 38, 63, 88, 113, 138, 163, 188, 213, 238);
+    	var thesholds = new Array(12, 37, 62, 87, 112, 137, 162, 187, 212, 237);
   		var winnerGets = new Array(8, 7, 6, 5, 4, 3, 2, 2, 1, 1, 0);
   		var losserLosses = new Array(8, 10, 13, 16, 20, 25, 30, 35, 40, 45, 50);
-  		var gap = Math.abs($scope.winnerScore - $scope.loserScore);
+  		var gap = Math.abs(parseInt($scope.winnerScore) - parseInt($scope.loserScore));
+  		var flag = new Boolean(false);
   		for (t in thesholds) {
   			if(gap > thesholds[t]) {
   				continue;
   			}
   			else {
-  				if($scope.winnerScore > $scope.loserScore) {
+  				flag = Boolean(true);
+  				if(parseInt($scope.winnerScore) > parseInt($scope.loserScore)) {
   					$scope.winnerGet = winnerGets[t];
   					$scope.loserLose = winnerGets[t];
   				}
@@ -29,10 +31,20 @@ app.controller('myCtr', function($scope) {
   				break;
   			}
   		}
+  		if(flag == false) {
+  			if(parseInt($scope.winnerScore) > parseInt($scope.loserScore)) {
+  					$scope.winnerGet = winnerGets.slice(-1)[0];
+  					$scope.loserLose = winnerGets.slice(-1)[0];
+  				}
+  				else {
+  					$scope.winnerGet = losserLosses.slice(-1)[0];
+  					$scope.loserLose = losserLosses.slice(-1)[0];
+  				}
+  		}
     };
     $scope.reset = function() {
-    	$scope.winner = "";
-    	$scope.losser = "";
+    	$scope.winner = "\n";
+    	$scope.losser = "\n";
     	$scope.winnerGet = "";
     	$scope.loserLose = "";
     };
